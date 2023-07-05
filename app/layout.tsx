8a8368d2faf431e8ba61281db7489fbdf96fad78
Niels Kersic <hi@niels.codes>
2023-07-05 15:43:36 +0200

    basic layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,29 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Team 20 🏆',
-  description: 'Hello world',
-}
+  title: "Team 20 🏆",
+  description: "Hello world",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div id="app-container">
+          <header id="global-header">
+            <h1 id="logo">Team 20, but #1 🏆</h1>
+          </header>
+          <main id="global-main">{children}</main>
+          <footer id="global-footer">Made with 🌳 by Team 20, Berlin</footer>
+        </div>
+      </body>
     </html>
-  )
+  );
 }
