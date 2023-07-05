246dcd2602637551e020aa81d49b53bc8d9bf846
Niels Kersic <hi@niels.codes>
2023-07-05 16:50:46 +0200

    new search design

--- app/search.tsx
+++ app/search.tsx
@@ -1,19 +1,19 @@
 "use client";
-import {
-  Button,
-  HStack,
-  Input,
-} from "@chakra-ui/react";
+import { Text, Button, HStack, Input, VStack } from "@chakra-ui/react";
 
 export default function Search() {
   return (
-    <HStack>
-      <Input
-        background="gray.200"
-        placeholder="What would you like to give away?"
-        pr="5"
-      />
-      <Button>Search</Button>
-    </HStack>
+    <VStack width="100%">
+      <HStack width="100%">
+        <Input
+          background="gray.200"
+          placeholder="What would you like to give away?"
+          pr="5"
+        />
+        <Button>Search</Button>
+      </HStack>
+      <Text>or...</Text>
+      <Button>Take a picture of your items</Button>
+    </VStack>
   );
 }
